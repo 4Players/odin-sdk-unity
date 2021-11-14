9dcbe3053e050bab057641e9506f6e71fcfeea66
Sven Paulsen <info@sven-paulsen.de>
2021-11-14 21:20:41 +0100

    cleanup and restructure samples

---
+++ Samples/Positional Audio/CustomUserDataJsonFormat.cs
@@ -0,0 +1,82 @@
+﻿using OdinNative.Odin;
+using System;
+using System.Text;
+using UnityEngine;
+
+namespace OdinNative.Unity.Samples
+{
+    [Serializable]
+    class CustomUserDataJsonFormat : IUserData
+    {
+        public string name;
+        public string seed;
+        public string status;
+        public int muted;
+        public string user;
+        public string renderer;
+        public string platform;
+        public string revision;
+        public string version;
+        public string buildno;
+
+        public CustomUserDataJsonFormat() : this("Unity Player", "online") { }
+        public CustomUserDataJsonFormat(string name, string status)
+        {
+            this.name = name;
+            this.seed = SystemInfo.deviceUniqueIdentifier;
+            this.status = status;
+            this.muted = 0;
+            this.user = string.Format("{0}.{1}", Application.companyName, Application.productName);
+            this.renderer = Application.unityVersion;
+            this.platform = string.Format("{0}/{1}", Application.platform, Application.unityVersion);
+            this.revision = "0";
+            this.version = Application.version;
+            this.buildno = Application.buildGUID;
+        }
+
+        public UserData ToUserData()
+        {
+            return new UserData(this.ToJson());
+        }
+
+        public static CustomUserDataJsonFormat FromUserData(UserData userData)
+        {
+            return JsonUtility.FromJson<CustomUserDataJsonFormat>(userData.ToString());
+        }
+
+        public static bool FromUserData(UserData userData, out CustomUserDataJsonFormat customUserData)
+        {
+            try
+            {
+                customUserData = JsonUtility.FromJson<CustomUserDataJsonFormat>(userData.ToString());
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                customUserData = null;
+                return false;
+            }
+        }
+
+        internal string GetAvatarUrl()
+        {
+            return string.Format("https://avatars.dicebear.com/api/bottts/{0}.svg?textureChance=0", this.seed);
+        }
+
+        public string ToJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
+
+        public override string ToString()
+        {
+            return this.ToJson();
+        }
+
+        public byte[] ToBytes()
+        {
+            return Encoding.UTF8.GetBytes(this.ToString());
+        }
+    }
+}
---
+++ Samples/Positional Audio/Odin3dTrigger.cs
@@ -0,0 +1,118 @@
+﻿using OdinNative.Odin;
+using OdinNative.Odin.Media;
+using OdinNative.Odin.Peer;
+using OdinNative.Odin.Room;
+using OdinNative.Unity;
+using OdinNative.Unity.Audio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace OdinNative.Unity.Samples
+{
+    public class Odin3dTrigger : MonoBehaviour
+    {
+        public GameObject prefab;
+        public List<GameObject> PeersObjects;
+        private Color LastCubeColor;
+        // Start is called before the first frame update
+        void Start()
+        {
+            OdinHandler.Instance.OnCreatedMediaObject.AddListener(Instance_OnCreatedMediaObject);
+            OdinHandler.Instance.OnDeleteMediaObject.AddListener(Instance_OnDeleteMediaObject);
+            OdinHandler.Instance.OnRoomLeft.AddListener(Instance_OnRoomLeft);
+
+            var SelfData = OdinHandler.Instance.GetUserData();
+            //Set Player
+            GameObject player = GameObject.FindGameObjectsWithTag("Player").FirstOrDefault();
+            if (player != null)
+            {
+                TextMesh label = player.GetComponentInChildren<TextMesh>();
+                label.text = CustomUserDataJsonFormat.FromUserData(SelfData)?.name ?? player.name;
+            }
+        }
+
+        GameObject CreateObject()
+        {
+            return Instantiate(prefab, new Vector3(0, 0.5f, 6), Quaternion.identity);
+        }
+
+        private void Instance_OnCreatedMediaObject(string roomName, ulong peerId, int mediaId)
+        {
+            Room room = OdinHandler.Instance.Rooms[roomName];
+            if (room == null || room.Self == null || room.Self.Id == peerId) return;
+
+            var peerContainer = CreateObject();
+
+            //Add PlaybackComponent to new dummy PeerCube
+            PlaybackComponent playback = OdinHandler.Instance.AssignPlaybackComponent(peerContainer, room.Config.Name, peerId, mediaId);
+            playback.OnPlaybackPlayingStatusChanged += TalkIndicator; // set function for talking indication by status
+
+            //Update Example
+            //playback.CheckPlayingStatusInUpdate = true; // set checking status per frame active
+            //InvokeRepeating Example
+            playback.CheckPlayingStatusAsInvoke = true; // set checking status as MonoBehaviour.InvokeRepeating active
+            playback.PlayingStatusDelay = 1.0f; // (default 0f)
+            playback.PlayingStatusRepeatingTime = 0.3f; // (default 0.2f)
+
+            //Some AudioSource test settings
+            playback.PlaybackSource.spatialBlend = 1.0f;
+            playback.PlaybackSource.rolloffMode = AudioRolloffMode.Linear;
+            playback.PlaybackSource.minDistance = 1;
+            playback.PlaybackSource.maxDistance = 10;
+
+            //set dummy PeerCube label
+            var data = CustomUserDataJsonFormat.FromUserData(room.RemotePeers[peerId]?.UserData);
+            playback.gameObject.GetComponentInChildren<TextMesh>().text = data == null ?
+                $"Peer {peerId} (Media {mediaId})" :
+                $"{data.name} (Peer {peerId} Media {mediaId})";
+
+            PeersObjects.Add(playback.gameObject);
+        }
+
+        private void TalkIndicator(PlaybackComponent playback, bool status)
+        {
+            Material cubeMaterial = playback.GetComponentInParent<Renderer>().material;
+            if (status)
+            {
+                LastCubeColor = cubeMaterial.color;
+                cubeMaterial.color = Color.green;
+            }
+            else
+                cubeMaterial.color = LastCubeColor;
+        }
+
+        private void Instance_OnDeleteMediaObject(int mediaId)
+        {
+            GameObject obj = PeersObjects.FirstOrDefault(o => o.GetComponent<PlaybackComponent>()?.MediaId == mediaId);
+            if (obj == null) return;
+
+            PeersObjects.Remove(obj);
+            Destroy(obj);
+        }
+
+        private void Instance_OnRoomLeft(RoomLeftEventArgs args)
+        {
+            GameObject[] objs = PeersObjects
+                .Where(o => o.GetComponent<PlaybackComponent>()?.RoomName == args.RoomName)
+                .ToArray();
+
+            if (objs.Length <= 0) return;
+
+            foreach (var obj in objs)
+            {
+                PeersObjects.Remove(obj);
+                Destroy(obj);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var obj in PeersObjects)
+                Destroy(obj);
+        }
+    }
+}
